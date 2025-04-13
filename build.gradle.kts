17fe3a69d8aa64301e6400384f8d78d5f7683223
Peter Becker <peter@concepta.au>
2025-04-13 15:03:32 +1000

    Working Gradle setup for publishing to Maven Central.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,17 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("jvm") version "2.1.20"
     `java-library`
     `maven-publish`
     signing
+    id("com.vanniktech.maven.publish") version "0.31.0"
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 group = "au.concepta"
-version = "1.0-SNAPSHOT"
 
 val junitVersion = "5.12.1"
 
@@ -31,37 +36,42 @@ kotlin {
     jvmToolchain(21)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            pom {
-                name = "Kotlin API for Playwright"
-                description = "A framework and utilities for using the Playwright automation framework with Kotlin."
-                url = "https://github.com/Concepta-AU/playwright-kotlin"
-                licenses {
-                    license {
-                        name = "The Unlicense"
-                        url = "https://unlicense.org/"
-                    }
-                }
-                developers {
-                    developer {
-                        name = "Peter Becker"
-                        email = "peter@concepta.au"
-                        organization = "Concepta Consulting Pty Ltd"
-                        organizationUrl = "https://concepta.au"
-                    }
-                }
-                scm {
-                    connection = "scm:git:git@github.com:Concepta-AU/playwright-kotlin.git"
-                    developerConnection = "scm:git:ssh://github.com:Concepta-AU/playwright-kotlin.git"
-                    url = "https://github.com/Concepta-AU/playwright-kotlin"
-                }
+// see https://vanniktech.github.io/gradle-maven-publish-plugin/central/ on how to configure the properties for this
+mavenPublishing {
+    configure(
+        KotlinJvm(
+            javadocJar = JavadocJar.Dokka("dokkaGenerate"),
+            sourcesJar = true,
+        )
+    )
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+    signAllPublications()
+
+    coordinates(project.group.toString(), rootProject.name, project.version.toString())
+
+    pom {
+        name = "Kotlin API for Playwright"
+        description = "A framework and utilities for using the Playwright automation framework with Kotlin."
+        url = "https://github.com/Concepta-AU/playwright-kotlin"
+        licenses {
+            license {
+                name = "The Unlicense"
+                url = "https://unlicense.org/"
             }
         }
+        developers {
+            developer {
+                name = "Peter Becker"
+                email = "peter@concepta.au"
+                organization = "Concepta Consulting Pty Ltd"
+                organizationUrl = "https://concepta.au"
+            }
+        }
+        scm {
+            connection = "scm:git:git@github.com:Concepta-AU/playwright-kotlin.git"
+            developerConnection = "scm:git:ssh://github.com:Concepta-AU/playwright-kotlin.git"
+            url = "https://github.com/Concepta-AU/playwright-kotlin"
+        }
     }
-}
-
-signing {
-    sign(publishing.publications["maven"])
 }
